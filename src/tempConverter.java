bdd93e8ea501897ca8ce7c5cb9e1825b06e36666
Shamak10 <124183731+Shamak10@users.noreply.github.com>
2023-02-24 16:36:01 -0500

    Update tempConverter.java

--- src/tempConverter.java
+++ src/tempConverter.java
@@ -1,15 +1,15 @@
 import java.util.Scanner;
-public class TempConverter {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+public class tempConverter {
+    public static void main(String[] args) {//Creating scanner to take input from the user
+        Scanner input = new Scanner("100");//Put in a value for celsius
         double celsius;
         do {
-            System.out.print("Enter the temperature in Celsius: ");
+            System.out.print("Enter the temperature in Celsius: ");//Taking temperature in C to covert it to F
             while (!input.hasNextDouble()) {
                 System.out.println("Invalid input. Please enter a valid number.");
                 input.next();
             }
-            celsius = input.nextDouble();
+            celsius = input.nextDouble();//Converting C to F
         } while (celsius < -273.15); // Loop until a valid temperature is entered
         double fahrenheit = celsius * 9 / 5 + 32;
         System.out.println(celsius + " degrees Celsius = " + fahrenheit + " degrees Fahrenheit.");
